6907d6683dc315b6b2f6054b597472cd1db09227
Shubham Raj <shubham443922@gmail.com>
2025-08-14 09:26:11 +0000

    committing main

---
+++ models/intermediate/int_station_distances.sql
@@ -0,0 +1,9 @@
+SELECT
+  a.OBJECTID AS station_a_id,
+  b.OBJECTID AS station_b_id,
+  ROUND(
+  ST_DISTANCE(ST_POINT(a.LONGITUDE, a.LATITUDE), ST_POINT(b.LONGITUDE, b.LATITUDE)) / 1000,  2) AS distance_km
+FROM {{ ref('stg_charging_stations_v1') }} a
+JOIN {{ ref('stg_charging_stations_v1') }} b
+  ON a.OBJECTID != b.OBJECTID
+WHERE ST_DISTANCE(ST_POINT(a.LONGITUDE, a.LATITUDE), ST_POINT(b.LONGITUDE, b.LATITUDE)) < 10000
